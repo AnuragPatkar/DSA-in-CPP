60dc7d43cf16e0ae014eab013cd4a119612f4a0f
AnuragPatkar <anuragpatkar786@gmail.com>
2025-03-03 14:09:17 +0530

    changes

--- DSA/first.cpp
+++ DSA/first.cpp
@@ -1966,11 +1966,7 @@ public:
     {
         front = NULL;
         rear = NULL;
-<<<<<<< HEAD
         if (q.front == NULL)
-=======
-        if (q.front == NULL) 
->>>>>>> 685987ef3b744ee1db9228189617fb9456d17525
         {
             return;
         }
@@ -2264,11 +2260,7 @@ Deque ::~Deque()
     delete[] ptr;
 } */
 
-<<<<<<< HEAD
 /* //! 14th Task: Deque using Doubly linked list
-=======
-/* //! 14th Task: Deque using linked list
->>>>>>> 685987ef3b744ee1db9228189617fb9456d17525
 #include <iostream>
 using namespace std;
 struct node
