b188937044c84d9dda3af2ebcec8346c6a127323
AnuragPatkar <anuragpatkar786@gmail.com>
2025-03-04 11:45:50 +0530

    add

--- DSA/first.cpp
+++ DSA/first.cpp
@@ -3205,4 +3205,126 @@ int main() {
     return 0;
 } */
 
+/* //! 19th Task: Graph Matrix
+#include <iostream>
+using namespace std;
+class Graph
+{
+private:
+    int v_count;
+    int e_count;
+    int **adj;
+
+public:
+    Graph(int vertices)
+    {
+        v_count = vertices;
+        e_count = 0;
+        adj = new int *[v_count];
+        for (int i = 0; i < v_count; i++)
+        {
+            adj[i] = new int[v_count];
+            for (int j = 0; j < v_count; j++)
+            {
+                adj[i][j] = 0;
+            }
+        }
+    }
+    void createGraph()
+    {
+        int u, v;
+        cout << "Enter number of Edges: ";
+        cin >> e_count;
+
+        if (e_count < 0 || e_count > (v_count * (v_count - 1)) / 2)
+        {
+            cout << "Invalid number of edges." << endl;
+            return;
+        }
+        for (int i = 0; i < e_count; i++)
+        {
+            cout << "Enter u and v(connected nodes): ";
+            cin >> u >> v;
+            if (u < 0 || v < 0 || u >= v_count || v >= v_count)
+            {
+                cout << "Invalid nodes." << endl;
+                i--;
+                continue;
+            }
+            if (u == v) // Optional: Self-loops allow nahi karne
+            {
+                cout << "Self-loops are not allowed. Try again." << endl;
+                i--;
+                continue;
+            }
+            adj[u][v] = 1;
+            adj[v][u] = 1;
+        }
+        cout << "Graph created successfully!" << endl;
+    }
+
+    void PrintGraphMatrix()
+    {
+        cout << "Adjacency Matrix: " << endl;
+        for (int i = 0; i < v_count; i++)
+        {
+            for (int j = 0; j < v_count; j++)
+            {
+                cout << adj[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    void PrintAdjacentNodes(int node)
+    {
+        if (node < 0 || node >= v_count)
+        {
+            cout << "Invalid input." << endl;
+            return;
+        }
+
+        cout << "Adjacent nodes of " << node << ": ";
+
+        if (isolated(node))
+            cout << "None" << endl;
+        else
+        {
+            for (int i = 0; i < v_count; i++)
+            {
+                if (adj[node][i] == 1)
+                {
+                    cout << i << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+    bool isolated(int node)
+    {
+        if (node < 0 || node >= v_count)
+        {
+            cout << "Invalid input." << endl;
+            return false;
+        }
+
+        for (int i = 0; i < v_count; i++)
+        {
+            if (adj[node][i] == 1)
+                return false;
+        }
+        return true;
+    }
+    ~Graph()
+    {
+        for (int i = 0; i < v_count; i++)
+        {
+            delete[] adj[i];
+        }
+        delete[] adj;
+        adj = NULL;
+    }
+}; */
+
+//!20 Task: Graph List Representation
+
 
--- ass 55/question.cpp
+++ ass 55/question.cpp
@@ -1,13 +1,115 @@
 //! Assignment-55: Graph Matrix
 
-//? 1. Define a class Graph using matrix representation with v_count, e_count and adj pointer as instance members.
+/* //? 1. Define a class Graph using matrix representation with v_count, e_count and adj pointer as instance members.
+#include <iostream>
+using namespace std;
+class Graph
+{
+    private:
+    int v_count;
+    int e_count;
+    int **adj;
+    public:
+}; */
 
-//? 2. In Question 1, define a method createGraph() to create and store adjacent node information.
+/* //? 2. In Question 1, define a method createGraph() to create and store adjacent node information.
+void createGraph()
+{
+    int u, v;
+    cout << "Enter number of Edges: ";
+    cin >> e_count;
 
-//? 3. In question 1, define a method to print graph matrix.
+    if (e_count < 0 || e_count > (v_count * (v_count - 1)) / 2)
+    {
+        cout << "Invalid number of edges." << endl;
+        return;
+    }
+    for (int i = 0; i < e_count; i++)
+    {
+        cout << "Enter u and v(connected nodes): ";
+        cin >> u >> v;
+        if (u < 0 || v < 0 || u >= v_count || v >= v_count)
+        {
+            cout << "Invalid nodes." << endl;
+            i--;
+            continue;
+        }
+        if (u == v) // Optional: Self-loops allow nahi karne
+        {
+            cout << "Self-loops are not allowed. Try again." << endl;
+            i--;
+            continue;
+        }
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+    cout << "Graph created successfully!" << endl;
+} */
 
-//? 4. In question 1, define a method to print all the adjacent nodes of a given node.
+/* //? 3. In question 1, define a method to print graph matrix.
+void PrintGraphMatrix()
+{
+    cout << "Adjacency Matrix: " << endl;
+    for (int i = 0; i < v_count; i++)
+    {
+        for (int j = 0; j < v_count; j++)
+        {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+} */
 
-//? 5. In question 1, define a method to check if a given node is isolated node.
+/* //? 4. In question 1, define a method to print all the adjacent nodes of a given node.
+void PrintAdjacentNodes(int node)
+{
+    if (node < 0 || node >= v_count)
+    {
+        cout << "Invalid input." << endl;
+        return;
+    }
 
-//? 6. In question 1, define a destructor to deallocates memory
+    cout << "Adjacent nodes of " << node << ": ";
+
+    if (isolated(node))
+        cout << "None" << endl;
+    else
+    {
+        for (int i = 0; i < v_count; i++)
+        {
+            if (adj[node][i] == 1)
+            {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    }
+} */
+
+/* //? 5. In question 1, define a method to check if a given node is isolated node.
+bool isolated(int node)
+{
+    if (node < 0 || node >= v_count)
+    {
+        cout << "Invalid input." << endl;
+        return false;
+    }
+
+    for (int i = 0; i < v_count; i++)
+    {
+        if (adj[node][i] == 1)
+            return false;
+    }
+    return true;
+} */
+
+/* //? 6. In question 1, define a destructor to deallocates memory
+~Graph()
+{
+    for (int i = 0; i < v_count; i++)
+    {
+        delete[] adj[i];
+    }
+    delete[] adj;
+    adj = NULL;
+} */
